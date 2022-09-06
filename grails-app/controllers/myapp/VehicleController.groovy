dfad80c7cce0ded5f6efd84ff201ed506a7440ea
Alex <ajscottleicester@gmail.com>
2022-09-06 10:51:20 +0100

    Configured index.gsp + added jQuery

--- grails-app/controllers/myapp/VehicleController.groovy
+++ grails-app/controllers/myapp/VehicleController.groovy
@@ -6,7 +6,7 @@ import static org.springframework.http.HttpStatus.NOT_FOUND
 import static org.springframework.http.HttpStatus.NO_CONTENT
 import static org.springframework.http.HttpStatus.OK
 import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY
-
+import myapp.Vehicle
 import grails.gorm.transactions.ReadOnly
 import grails.gorm.transactions.Transactional
 
